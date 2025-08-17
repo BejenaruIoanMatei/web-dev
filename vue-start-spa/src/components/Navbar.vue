55529d988fc0c5137f4ae8d10c17f2d1cb2d6791
BejenaruIoanMatei <bejematei@gmail.com>
2025-08-17 14:26:33 +0300

    10. VUE.js Course - Vue CLI

---
+++ vue-start-spa/src/components/Navbar.vue
@@ -0,0 +1,54 @@
+<template>
+    <nav
+        v-bind:class="[`navbar-${theme}`, `bg-${theme}`, 'navbar', 'navbar-expand-lg']"
+    >
+        <div class="container-fluid">
+        <a href="#" class="navbar-brand">My Vue</a>
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li v-for="(page, index) in pages" class="nav-item" v-bind:key="index">
+                <navbar-link
+                    :page="page"
+                    :isActive="activePage === index"
+                    @click.prevent="navLinkClick(index)"
+                >
+
+                </navbar-link>
+            </li> <!--:href without v-bind works-->
+                <!--v-on:click = @click-->
+        </ul>
+        <form class="d-flex">
+            <button 
+                class="btn btn-primary"
+                @click.prevent="changeTheme()">
+                    Toggle Navbar
+            </button>
+        </form>
+        </div>
+    </nav>
+</template>
+
+<script>
+import NavbarLink from './NavbarLink.vue';
+
+export default {
+    components:{
+        NavbarLink},
+    props: ['pages', 'activePage', 'navLinkClick'],
+    data(){
+        return {
+            theme: 'light',
+        };
+    },
+    methods: {
+        changeTheme() {
+            let theme = 'light';
+
+            if(this.theme == 'light'){
+                theme = 'dark';
+            }
+
+            this.theme = theme;
+        }
+    }
+}
+</script>
